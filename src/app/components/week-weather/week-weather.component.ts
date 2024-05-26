12ad1ff3966c08ca7be86afa95874c90f4c5fcf1
Łukasz <43967017+magik9907@users.noreply.github.com>
2024-05-26 23:41:32 +0200

    week stats

--- src/app/components/week-weather/week-weather.component.ts
+++ src/app/components/week-weather/week-weather.component.ts
@@ -2,17 +2,26 @@ import { CityWeather, WeekWeatherForecast } from '@/types';
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   input,
   signal,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { WeatherChartsComponent } from '@components/weather-charts/weather-charts.component';
 import { PurePipe } from '@pipes/pure.pipe';
+import { SpeedPipe } from '@pipes/speed.pipe';
+import { TemperaturePipe } from '@pipes/temperature.pipe';
 
 @Component({
   selector: 'app-week-weather',
   standalone: true,
-  imports: [PurePipe, WeatherChartsComponent, MatButtonModule],
+  imports: [
+    PurePipe,
+    WeatherChartsComponent,
+    MatButtonModule,
+    TemperaturePipe,
+    SpeedPipe,
+  ],
   templateUrl: './week-weather.component.html',
   styleUrl: './week-weather.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,15 +30,34 @@ export class WeekWeatherComponent {
   data = input.required<CityWeather>();
   selectedInfo = signal('temperature_day');
 
+  avgPressure = computed(() => {
+    const week = this.data().week;
+    const sum = week.reduce((buff, next) => buff + next.pressure, 0);
+    return sum / week.length;
+  });
+
+  avgTemperature = computed(() => {
+    const week = this.data().week;
+    const sum = week.reduce((buff, next) => buff + next.temperature_day, 0);
+    return sum / week.length;
+  });
+  avgWind = computed(() => {
+    const week = this.data().week;
+    const sum = week.reduce((buff, next) => buff + next.wind, 0);
+    return sum / week.length;
+  });
+
   getWeekStatsForKey(
     key: string,
     data: WeekWeatherForecast
   ): { key: Date; value: number }[] {
-    return data
+    return (
+      data
         //eslint-disable-next-line
         .map((v: any) => {
-        return { key: v.date, value: v[key] };
-      })
-      .slice(0, 7);
+          return { key: v.date, value: v[key] };
+        })
+        .slice(0, 7)
+    );
   }
 }
